ef52a9877e366e8e280fdb186558319feb1f5d00
yoshi <yoshi@tech-camp-9-021-no-MacBook-Air.local>
2017-05-12 12:37:14 +0900

    searchまで終了

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,2 +1,13 @@
 class ProductsController < RankingController
+  def index
+    @products = Product.order('id ASC').limit(20)
+  end
+
+  def show
+    @product = Product.find(params[:id])
+  end
+
+  def search
+    @products = Product.where('title LIKE(?)',"%#{params[:keyword]}%").limit(20)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,2 +1,9 @@
 TechReviewSite::Application.routes.draw do
+  resources :products, only: :show do
+    resources :reviews,only: [:new, :create]
+    collection do
+      get 'search'
+    end
+  end
+  root 'products#index'
 end
