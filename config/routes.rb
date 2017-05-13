f2339bf33367ec0926ac8efa86ca24885607a014
yoshi <yoshi@tech-camp-9-021-no-MacBook-Air.local>
2017-05-13 13:17:05 +0900

    my page実装完了

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 TechReviewSite::Application.routes.draw do
   devise_for :users
+  resources :users, only: :show
   resources :products, only: :show do
     resources :reviews,only: [:new, :create]
     collection do
