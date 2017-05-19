143975bd0c6c6906e36fb3f7823563cad7176e20
yoshi <yoshi@tech-camp-9-021-no-MacBook-Air.local>
2017-05-19 14:24:17 +0900

    ランキング機能追加

--- app/controllers/ranking_controller.rb
+++ app/controllers/ranking_controller.rb
@@ -14,29 +14,36 @@ class RankingController < ApplicationController
 
     # 以下評価のランキング
 
-    # rate_product_ids = Review.order("product_id ASC").pluck(:product_id).uniq!
+    rate_product_ids = Review.order("product_id ASC").pluck(:product_id).uniq!
 
-    # reviews = [] #product_id毎のrateを集計
-    # rate_product_ids.each do |p_id|
+    reviews = [] #product_id毎のrateを集計
+    rate_product_ids.each do |p_id|
 
-    #   rate_sum = Review.where(product_id: p_id).pluck(:rate).inject(0) { |sum, i| sum + i }
+      rate_sum = Review.where(product_id: p_id).pluck(:rate).inject(0) { |sum, i| sum + i }
 
-    #   reviews << rate_sum
-    # end
+      reviews << rate_sum
+    end
+    #"count_product_id DESC"ではなく"product_id ASC"に書き換えた
+    review_sum = Review.group(:product_id).order('product_id ASC').count(:product_id).to_a
 
-    # review_sum = Review.group(:product_id).order('count_product_id DESC').count(:product_id).to_a
+    last_answer = []
+    reviews.zip(review_sum).each do |ra_sum, re_sum|
+      last_ha = {}
+      answer = ra_sum.quo(re_sum[1].to_f)
 
-    # last_answer = []
-    # reviews.zip(review_sum).each do |ra_sum, re_sum|
-    #   last_ha = {}
-    #   answer = ra_sum.quo(re_sum[1].to_f)
+      last_ha[:pro_id] = re_sum[0]
+      last_ha[:ave] = answer
+      last_answer << last_ha
 
-    #   last_ha[:pro_id] = re_sum[0]
-    #   last_ha[:ave] = answer
-    #   last_answer << last_ha
-    # end
+    end
+
+    last_answer.sort_by{|k,v| v }
+    
+    binding.pry
 
     # 以下最後にaveの順に並び替えてそのハッシュの配列からpro_idの情報だけを上から５個取り出して配列にする。そして最後にReview〜みたいな形でデータを取り出す。
+
+
 
 
   end
--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,8 @@ TechReviewSite::Application.routes.draw do
   devise_for :users
   resources :users, only: :show
   resources :products, only: :show do
-    resources :reviews,only: [:new, :create,:edit,:update,:destroy] do
-      resources :likes, only: [:create, :destroy]
+    resources :reviews,only: [:new, :create,:edit,:update,:destroy]  do 
+      resources :likes, only: [:create, :destroy],shallow: true #shallow を追加してみた
 
     collection do
       get 'search'
